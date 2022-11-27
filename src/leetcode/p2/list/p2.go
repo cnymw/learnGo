5aa8b054fc6d5c18700c9feba4ddcb5adcb22dae
benjamiyang <benjaminymw@foxmail.com>
2022-11-27 20:15:19 +0800

    list

--- src/leetcode/p2/list/p2.go
+++ src/leetcode/p2/list/p2.go
@@ -6,25 +6,29 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var tail, head *ListNode
+	// head 链表首节点
+	// tail 链表尾节点
+	var head, tail *ListNode
 
-	carry := 0
+	// 记录进位值
+	var carry int
 
+	// 持续遍历链表 l1,l2，直到两个链表都遍历完毕
 	for l1 != nil || l2 != nil {
-		v1, v2 := 0, 0
+		n1, n2 := 0, 0
 		if l1 != nil {
-			v1 = l1.Val
+			n1 = l1.Val
 			l1 = l1.Next
 		}
-
 		if l2 != nil {
-			v2 = l2.Val
+			n2 = l2.Val
 			l2 = l2.Next
 		}
 
-		sum := v1 + v2 + carry
+		sum := n1 + n2 + carry
 		sum, carry = sum%10, sum/10
 
+		// 如果结果链表为空，那么创建链表首节点
 		if head == nil {
 			head = &ListNode{Val: sum}
 			tail = head
@@ -33,8 +37,11 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			tail = tail.Next
 		}
 	}
-	if carry > 0 {
+
+	// 如果进位值不为 0，那么在链表尾节点新增一个节点，用于记录进位值
+	if carry != 0 {
 		tail.Next = &ListNode{Val: carry}
 	}
+	// 返回链表首节点
 	return head
 }
